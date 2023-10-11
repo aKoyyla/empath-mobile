921ee561e42de16ff89114babfc45b90d2f2ff15
Div Khare <divyanshukhare@gmail.com>
2023-10-11 15:57:15 -0400

    fix for userProfile

--- EmpathMobile/screens/UserProfileScreen/UserProfile.js
+++ EmpathMobile/screens/UserProfileScreen/UserProfile.js
@@ -1,71 +1,20 @@
-// import React, { useState } from 'react';
-// import { View, Text, TouchableOpacity, Image } from 'react-native';
-// import { launchImageLibrary } from 'react-native-image-picker';
-// import styles from './styles';
-// import { getSignedUrl } from '../../api/authApi';
-// const RNFetchBlob = require('rn-fetch-blob').default;
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
+import styles from './styles';
+import { getSignedUrl } from '../../api/authApi';
+const RNFetchBlob = require('rn-fetch-blob').default;
 
-// const avatarImages = [
-//   require('../../assets/images/UserProfile/User1.png'),
-//   require('../../assets/images/UserProfile/User2.png'),
-//   require('../../assets/images/UserProfile/User3.png'),
-//   require('../../assets/images/UserProfile/User4.png'),
-//   require('../../assets/images/UserProfile/User5.png'),
-//   require('../../assets/images/UserProfile/User6.png'),
-//   require('../../assets/images/UserProfile/User7.png'),
-//   require('../../assets/images/UserProfile/User8.png'),
-// ];
-
-// const UserProfile = () => {
-//   const [selectedAvatar, setSelectedAvatar] = useState(avatarImages[0]);
-//   const [userAvatarUrl, setUserAvatarUrl] = useState('');
-
-//     launchImageLibrary(options, async response => {
-//       if (response.didCancel) {
-//         console.log('User cancelled image picker');
-//       } else if (response.error) {
-//         console.log('ImagePicker Error: ', response.error);
-//       } else {
-//         console.log('ImagePicker Response:', response);
-
-//         const asset = response.assets[0];
-//         const fileName = asset.fileName || `${new Date().getTime()}.jpg`;
-//         console.log('Asset Details:', {
-//           fileName: asset.fileName,
-//           type: asset.type,
-//           uri: asset.uri,
-//         });
-
-//         let fileType = asset.type;
-//         const fileExtension = fileName.split('.').pop().toLowerCase();
-//         console.log('Deduced File Extension:', fileExtension);
-
-//         switch (
-//           fileExtension
-//         ) {
-//           case 'jpg':
-//           case 'jpeg':
-//             fileType = 'image/jpeg';
-//             break;
-//           case 'png':
-//             fileType = 'image/png';
-//             break;
-//           case 'gif':
-//             fileType = 'image/gif';
-//             break;
-//           default:
-//             console.log('Unknown file extension received:', fileExtension);
-//             return;
-//         }
-
-//         getSignedUrl(fileName, fileType)
-//           .then(signedUrlData => {
-//             const { signedRequest } = signedUrlData;
-//           };
-//   };
-
-//   export default UserProfile;
-
+const avatarImages = [
+  require('../../assets/images/UserProfile/User1.png'),
+  require('../../assets/images/UserProfile/User2.png'),
+  require('../../assets/images/UserProfile/User3.png'),
+  require('../../assets/images/UserProfile/User4.png'),
+  require('../../assets/images/UserProfile/User5.png'),
+  require('../../assets/images/UserProfile/User6.png'),
+  require('../../assets/images/UserProfile/User7.png'),
+  require('../../assets/images/UserProfile/User8.png'),
+];
 
 const UserProfile = () => {
   const [selectedAvatar, setSelectedAvatar] = useState(avatarImages[0]);
@@ -108,46 +57,103 @@ const UserProfile = () => {
         }
 
         getSignedUrl(fileName, fileType)
-          .then((signedUrlData) => {
-            const { signedRequest } = signedUrlData;
+        .then(signedUrlData => {
+          const { signedRequest } = signedUrlData;
+          setUserAvatarUrl(asset.uri);
 
-            // Upload the image using RNFetchBlob or another library
-            // You need to implement the upload logic here
-            // Example code (use your preferred library for uploading):
-            RNFetchBlob.fetch(
-              'PUT',
-              signedRequest,
-              {
-                'Content-Type': fileType,
-              },
-              RNFetchBlob.wrap(asset.uri),
-            )
-              .then((response) => {
-                if (response.info().status === 200) {
-                  // Image successfully uploaded
-                  setUserAvatarUrl(signedRequest);
-                } else {
-                  console.log('Image upload failed');
-                }
-              })
-              .catch((error) => {
-                console.log('Image upload error:', error);
-              });
-          })
-          .catch((error) => {
-            console.log('Error getting signed URL:', error);
+          return fetch(signedRequest, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': file.type,
+            },
+            body: asset.uri,
           });
+        })
+        .then(uploadResponse => {
+          console.log('uploadResponse:', uploadResponse);
+          if (uploadResponse.info().status !== 200) {
+            console.error('Failed to upload image to S3. AWS Response:', uploadResponse.text());
+            return;
+          }
+          //setUserAvatarUrl(uploadResponse.data.url);  // Assuming the URL of the uploaded image is returned in the response
+        })
+        .catch(error => {
+          console.error('Error during S3 upload:', error);
+        });
       }
     });
   };
 
   return (
     <View style={styles.container}>
-      <Image source={selectedAvatar} style={styles.avatar} />
-      <TouchableOpacity onPress={launchImagePicker}>
-        <Text>Change Avatar</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.topSection}>
+        <Text style={styles.title}>One Last Thing</Text>
+        <Text style={styles.subtitle}>Choose your avatar. Why not?</Text>
+        <View
+          style={[
+            styles.largeAvatarWrapper,
+            selectedAvatar === selectedAvatar && styles.selectedLargeAvatar,
+          ]}>
+          <Image
+            source={userAvatarUrl ? {uri: userAvatarUrl} : selectedAvatar}
+            style={styles.largeAvatar}
+          />
+        </View>
+
+
+        <View style={styles.row}>
+          {avatarImages.slice(0, 4).map((img, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => setSelectedAvatar(img)}>
+              <View
+                style={[
+                  styles.avatarWrapper,
+                  img === selectedAvatar && styles.selectedAvatar,
+                ]}>
+                <Image source={img} style={styles.avatar} />
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={styles.row}>
+          {avatarImages.slice(4, 8).map((img, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => setSelectedAvatar(img)}>
+              <View
+                style={[
+                  styles.avatarWrapper,
+                  img === selectedAvatar && styles.selectedAvatar,
+                ]}>
+                <Image source={img} style={styles.avatar} />
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View style={styles.orContainer}>
+          <View style={styles.orDividerLeft} />
+          <Text style={styles.orText}>OR</Text>
+          <View style={styles.orDividerRight} />
+        </View>
+
+        <TouchableOpacity
+          style={styles.chooseGalleryButton}
+          onPress={handleChooseFromGallery}>
+          <Text style={styles.chooseGalleryButtonText}>
+            Choose from Gallery
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.button2Text}>I'll do this later</Text>
+        </TouchableOpacity>
+        <Text style={styles.note}>
+          You can change your avatar later from the "Setting" page at any point
+          in time.
+          </Text>
+          </View>
+          </View>
   );
 };
 
